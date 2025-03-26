0c509957ef4ce2cc490fc266c606647c7e3a1811
Reese <reese.westerhoff@gmail.com>
2025-03-26 17:04:22 +0100

    display docs if not in production

--- backend/services/ai/index.ts
+++ backend/services/ai/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { routes } from "./src/routes";
 import { errorHandler } from "./src/middleware/error";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import { api } from "./api-spec/spec";
 
 dotenv.config();
 
@@ -13,6 +15,11 @@ const router = express.Router();
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
+
+// only use docs if not in production
+if (!process.env.NODE_ENV) {
+  app.use("/documentation", swaggerUi.serve, swaggerUi.setup(api));
+}
 
 routes(router);
 
